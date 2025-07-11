a2ac6fb4d2a2b49580cc26aec34c5a1ecf14ebfe
arif0718 <arifansari90467@gmail.com>
2025-07-11 15:25:23 +0530

    up

--- server.js
+++ server.js
@@ -12,20 +12,20 @@ dotenv.config();
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
-// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
-//   app.listen(8081, () => {
-//     console.log("Server started");
-//   });
-// });
-
-mongoose
-  .connect(
-"mongodb+srv://arifansari90467:ysMmX6QmV9qZYR4N@cluster0.9rhplrk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .then(() => {
-    app.listen(8081, () => {
-      console.log("Server started");
-    });
+mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
+  app.listen(8081, () => {
+    console.log("Server started");
+  });
 });
+
+// mongoose
+//   .connect(
+// "mongodb+srv://arifansari90467:ysMmX6QmV9qZYR4N@cluster0.9rhplrk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+//   )
+//   .then(() => {
+//     app.listen(8081, () => {
+//       console.log("Server started");
+//     });
+// });
 
 app.use("/api/users", userRouter);
